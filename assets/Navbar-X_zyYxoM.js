d656efe6251d0c5a5c5de4f7aa23dd2b2295a351
Jeshiko J <chandranjeshiko@gmail.com>
2026-07-06 08:04:24 +0530

    Updates

--- assets/Navbar-X_zyYxoM.js
+++ assets/Navbar-X_zyYxoM.js
@@ -1,4 +1,4 @@
-import{j as P,r as bt}from"./index-BCcQuKCS.js";import{g as St,S as yt}from"./ScrollTrigger-CiEuWA-R.js";/*!
+import{j as P,r as bt}from"./index-BI62N2bn.js";import{g as St,S as yt}from"./ScrollTrigger-CiEuWA-R.js";/*!
  * SplitText 3.15.0
  * https://gsap.com
  *
